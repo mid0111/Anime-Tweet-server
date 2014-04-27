e22637b5ba5dd408776984ef5ad000fbb4c75374
mid0111 <m.tajima0111@gmail.com>
2014-04-27 16:19:06 +0900

    Twitterストリーミング処理を追加.

--- app.js
+++ app.js
@@ -18,11 +18,30 @@ app.get('/', routes.index);
 
 server.listen(config.port);
 
-// socket open
+//
+// Twitterストリーミング
+//
+var twit = require('./config/twitter');
+
+// Twitter検索条件
+// TODO: Anime map apiから取得した値に変更（現状は仮で固定値を指定）
+var tracks = 'node, express, mocha, google';
+
 io.sockets.on('connection', function (socket) {
-  var data = {
-    userId: 'user0001',
-    message: 'hello'
-  };
-  socket.emit('tweet', data);
+  console.log('socket connected.');
+  // Twitterストリーム作成
+  var twitStream = twit.stream('statuses/filter', {'track': tracks}, function(stream) {
+    stream.on('data', function (data) {
+      console.log('tweets: ' + JSON.stringify(data));
+      var twitterMessage = {
+        userId: data.user.name,
+        message: data.text
+      };
+      socket.emit('tweet', twitterMessage);
+    });
+  });
+  socket.on('disconnect', function () {
+    io.sockets.emit('user disconnected');
+  });
 });
+
--- test/socket.test.spec.js
+++ test/socket.test.spec.js
@@ -4,19 +4,34 @@ var expect = chai.expect;
 
 var client = require('socket.io-client');
 
-describe('socket接続', function() {
+describe('socket', function() {
 
-  it('socketに接続できること', function() {
-    var socket = client.connect('http://localhost:3000');
-  });
+  // describe('socket接続', function() {
+  //   it('socketに接続できること', function() {
+  //     var socket = client.connect('http://localhost:3000');
+  //     socket.emit('disconnect', null);
+  //   });
+  // });
 
-  it('socketに接続できること', function(done) {
-    var socket = client.connect('http://localhost:3000');
-    socket.on('tweet', function (data) {
-      expect(data.userId).not.to.be.null;
-      expect(data.message).not.to.be.null;
+  describe('socketデータ送信', function() {
+    var socket;
+
+    before(function(done) {
+      socket = client.connect('http://localhost:3000');
       done();
     });
-  });
 
+    after(function() {
+      socket.emit('disconnect', null);
+    });
+
+    it('期待する形式でsocketからデータを受信できること', function(done) {
+
+      socket.on('tweet', function (data) {
+        expect(data.userId).not.to.be.null;
+        expect(data.message).not.to.be.null;
+        done();
+      });
+    });
+  });
 });
