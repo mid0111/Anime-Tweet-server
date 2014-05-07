bcc80b438ca863bba5134c92b9f906087e090ae9
mid0111 <m.tajima0111@gmail.com>
2014-05-07 23:29:25 +0900

    DB設定を環境ごとに切り分ける処理を追加. `grunt test`実行時はtest用の設定で動作するように定義を変更.

--- config/db.js
+++ config/db.js
@@ -1,16 +1,41 @@
 // DB操作用の設定定義.
-var  Bookshelf  = require('bookshelf');
 
+var env = process.env.NODE_ENV || 'development';
+
+var config = {
+  development: {
+    host     : '127.0.0.1',
+    user     : 'postgres',
+    password : 'postgres',
+    database : 'anime_tweet_dev'
+  },
+
+  test: {
+    host     : '127.0.0.1',
+    user     : 'postgres',
+    password : 'postgres',
+    database : 'anime_tweet_test'
+  },
+
+  production: {
+    host     : '127.0.0.1',
+    user     : process.env.DB_USER,
+    password : process.env.DB_PASSWORD,
+    database : 'anime_tweet'
+  }
+};
+
+var  Bookshelf  = require('bookshelf');
 exports.initialize = function() {
   if(Bookshelf.PG === undefined) {
     // Bookshelf初期化
     Bookshelf.PG = Bookshelf.initialize({
       client: 'pg',
       connection: {
-        host     : '127.0.0.1',
-        user     : 'postgres',
-        password : 'postgres',
-        database : 'anime_tweet_test',
+        host     : config[env].host,
+        user     : config[env].user,
+        password : config[env].password,
+        database : config[env].database,
         charset  : 'UTF8_GENERAL_CI'
       }
     });
