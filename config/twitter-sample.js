93a3791047f1cb48074a7f25c824491a3e9b7f16
mid0111 <m.tajima0111@gmail.com>
2014-05-10 15:56:31 +0900

    Twitter検索条件をDBから取得するように変更した.

--- config/twitter-sample.js
+++ config/twitter-sample.js
@@ -1,11 +1,8 @@
 // twitter settings
-var twitter = require('ntwitter');
-var twit = new twitter({
+module.exports = {
   consumer_key: 'xxx',
   consumer_secret: 'yyy',
   access_token_key: 'zzz',
   access_token_secret: 'www'
-});
-
-module.exports = twit;
+};
 
